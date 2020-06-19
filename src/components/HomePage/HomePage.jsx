b6f5de20c7fd2fc5e6eeda3f0351b4a6d1e58932
imhul <blashirk@gmail.com>
2020-06-20 01:05:36 +0300

    home page style update

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -18,7 +18,6 @@ const HomePage = () => {
             >
                 <Layout>
                     <h1 className="hero">proto-mass</h1>
-                    <h2 className="hero-desc">sandbox</h2>
                 </Layout>
     </WindowSizeListener>
 }
